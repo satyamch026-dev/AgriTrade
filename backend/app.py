f0c9bf52e7d3f2be14a1ba935dd10e267ee38f67
satyamch026-dev <satyamch026@gmail.com>
2026-09-17 18:41:56 +0530

    Fix Railway Flask port

--- backend/app.py
+++ backend/app.py
@@ -1057,7 +1057,8 @@ def server_error(e):
 
 if __name__ == '__main__':
 
-    app.run(
-        debug=True,
-        port=5000
-    )
+   import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
